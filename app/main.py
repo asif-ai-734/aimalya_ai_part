e8d03bf0e49d9f698fc4b0f10b2e77d6ae2319e3
Asifuzzaman Asif <asifuzzamanasif734@gmail.com>
2026-05-12 12:19:00 +0600

    Cors error solved

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ from app.routes import business_setup
 from app.routes import business_management
 from app.routes import goals_set_up_py
 from app.services.place_loader import PlaceDataNotFound
+from fastapi.middleware.cors import CORSMiddleware
 
 # from app.services.competitor_loader import find_competitors_from_place_json
 # from app.services.place_loader import load_place_data
@@ -25,7 +26,13 @@ app= FastAPI(
     description= "AI Powere business review analytics",
     version= "1.0.0"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://reviewiq-two.vercel.app", "http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
 app.include_router(business_setup.router)
 app.include_router(goals_set_up_py.router)
 app.include_router(overview.router)
